3e84f376591835b062883b54afafad278c9f9eb6
Frederic Houdemer <f.houdemer@gmail.com>
2024-12-06 00:53:28 +0100

    :sparkles: add second part for day 5

--- day-05/main.go
+++ day-05/main.go
@@ -21,10 +21,6 @@ func getLines(filename string) []string {
 }
 
 func secondPart(lines []string) int {
-	return len(lines)
-}
-
-func firstPart(lines []string) int {
 	middlesCount := 0
 	startSorting := false
 	rules := make([][][2]int, 100)
@@ -71,7 +67,19 @@ func firstPart(lines []string) int {
 		}
 
 		// Is line in correct order ?
-		if checkLine(numbers, rules) {
+		checked, matched := checkLine(numbers, rules)
+		ogLineInError := matched != -1
+
+		// Naive approeach.. switching invalid values and checking again until we got a valid line.
+		for matched != -1 {
+			tmp := numbers[checked]
+			numbers[checked] = numbers[matched]
+			numbers[matched] = tmp
+			checked, matched = checkLine(numbers, rules)
+		}
+
+		// If line was originally in error.
+		if ogLineInError {
 			middlesCount += numbers[len(numbers)/2]
 		}
 	}
@@ -79,7 +87,61 @@ func firstPart(lines []string) int {
 	return middlesCount
 }
 
-func checkLine(numbers []int, rules [][][2]int) bool {
+func firstPart(lines []string) int {
+	middlesCount := 0
+	startSorting := false
+	rules := make([][][2]int, 100)
+	numbers := make([]int, 0, 99)
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		if line[0] == 'X' {
+			startSorting = true
+			continue
+		}
+
+		// Building sorting rules.
+		if !startSorting {
+			a, errA := strconv.Atoi(string(line[0]) + string(line[1]))
+			b, errB := strconv.Atoi(string(line[3]) + string(line[4]))
+			check(errA)
+			check(errB)
+
+			if len(rules[a]) == 0 {
+				rules[a] = make([][2]int, 0, 100)
+			}
+			if len(rules[b]) == 0 {
+				rules[b] = make([][2]int, 0, 100)
+			}
+
+			rules[a] = append(rules[a], [2]int{a, b})
+			rules[b] = append(rules[b], [2]int{a, b})
+
+			continue
+		}
+
+		// Converting line in numbers.
+		splits := strings.Split(line, ",")
+		numbers = numbers[:len(splits)]
+
+		for i, s := range splits {
+			n, errN := strconv.Atoi(s)
+			check(errN)
+			numbers[i] = n
+		}
+
+		// Is line in correct order ?
+		if _, matched := checkLine(numbers, rules); matched == -1 {
+			middlesCount += numbers[len(numbers)/2]
+		}
+	}
+
+	return middlesCount
+}
+func checkLine(numbers []int, rules [][][2]int) (int, int) {
 	// Check each number on the line
 	for i, n := range numbers {
 		// Check each rule for this number
@@ -89,7 +151,7 @@ func checkLine(numbers []int, rules [][][2]int) bool {
 				// Check for upper values
 				for j := i; j >= 0; j-- {
 					if numbers[j] == r[1] {
-						return false
+						return i, j
 					}
 				}
 			} else {
@@ -97,14 +159,14 @@ func checkLine(numbers []int, rules [][][2]int) bool {
 				// Check all numbers on the line before
 				for j := i; j < len(numbers); j++ {
 					if numbers[j] == r[0] {
-						return false
+						return i, j
 					}
 				}
 			}
 		}
 	}
 
-	return true
+	return -1, -1
 }
 
 func main() {
